d64527f168557308711f943d5fcfc2f4254872f7
katej783 <77684635+katej783@users.noreply.github.com>
2021-01-28 17:13:15 +0000

    replace value

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
-data = pd.read_csv("goodreadsbooks.csv")
-print(data)
+df = pd.read_csv("goodreadsbooks.csv")
+
+print(df)
+
+print(df.isnull().sum())
+
+print(df.dtypes)
+
+print(df.describe())
+
+df.replace(to_replace='J.K. Rowling/Mary GrandPré', value = 'J.K. Rowling', inplace=True)
+
+print(df)
+
 
 
