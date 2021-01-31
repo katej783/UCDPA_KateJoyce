e1fb63159f55d8e4a44645193c50a70e63889b58
katej783 <77684635+katej783@users.noreply.github.com>
2021-01-31 16:57:53 +0000

    sort and iloc

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 book = pd.read_csv("GoodreadsBooks.csv", error_bad_lines=False)
 
 print(book.isnull().sum())
-
 print(book.dtypes)
+books = book.drop(columns=['bookID', 'isbn', 'isbn13', 'publisher'], axis=1)
+print(books.head())
+print(books.shape)
 
-books = book.drop(columns=['isbn', 'isbn13', 'publication_date', 'publisher'], axis=1)
+missing_values_count = books.isnull().sum()
+print(missing_values_count)
 
-print(books.describe())
+drop_duplicates = books.drop_duplicates()
+print(books.shape,drop_duplicates.shape)
 
 books.replace(to_replace='J.K. Rowling/Mary GrandPré', value='J.K. Rowling', inplace=True)
 
-print(books.isnull().sum())
+total_cells = np.product(books.shape)
+total_missing = (missing_values_count.sum())
+percent_missing = (total_missing/total_cells)
+print(percent_missing)
 
-top_rated = books.sort_values('average_rating', ascending=False)
+most_common_authors = books['authors'].value_counts()[:40]
+print(most_common_authors)
+print(most_common_authors['John Grisham'])
 
-print(top_rated)
+top_books = books[books['ratings_count'] > 100000]
+top_20 = top_books.sort_values('average_rating', ascending=False).head(20)
+print(top_20)
+
+most_rated = top_books.sort_values('ratings_count', ascending=False).head(20)
+print(most_rated)
+
+top_5 = top_20.iloc[[0,1,2,3,4],[0,1,2]]
+print(top_5)
