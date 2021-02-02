8425b1c4553211114df3887327ffec8a78cf0fdc
katej783 <77684635+katej783@users.noreply.github.com>
2021-02-02 19:37:48 +0000

    with plt&sns

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 book = pd.read_csv("GoodreadsBooks.csv", error_bad_lines=False)
 
 print(book.isnull().sum())
@@ -13,13 +15,13 @@ missing_values_count = books.isnull().sum()
 print(missing_values_count)
 
 drop_duplicates = books.drop_duplicates()
-print(books.shape,drop_duplicates.shape)
+print(books.shape, drop_duplicates.shape)
 
 books.replace(to_replace='J.K. Rowling/Mary GrandPré', value='J.K. Rowling', inplace=True)
 
 total_cells = np.product(books.shape)
 total_missing = (missing_values_count.sum())
-percent_missing = (total_missing/total_cells)
+percent_missing = (total_missing / total_cells)
 print(percent_missing)
 
 most_common_authors = books['authors'].value_counts()[:40]
@@ -33,5 +35,34 @@ print(top_20)
 most_rated = top_books.sort_values('ratings_count', ascending=False).head(20)
 print(most_rated)
 
-top_5 = top_20.iloc[[0,1,2,3,4],[0,1,2]]
+top_5 = top_20.iloc[[0, 1, 2, 3, 4], [0, 1, 2]]
 print(top_5)
+
+language = books.groupby('language_code')['title'].count()
+print(language)
+
+sns.barplot(x=most_common_authors, y=most_common_authors.index)
+plt.subplots_adjust(left=0.3)
+plt.title("40 Authors with Most Books")
+plt.subplots_adjust(top=0.9)
+plt.show()
+
+fig, ax = plt.subplots()
+x = books['title'].head(5)
+y = books['num_pages'].head(5)
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.4)
+plt.title("Pages")
+plt.subplots_adjust(top=0.9)
+ax.bar(x, y)
+plt.show()
+
+fig, ax = plt.subplots()
+x2=top_20['title'].head(10)
+y2=books['average_rating'].head(10)
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.4)
+plt.title("Top Rated Books")
+plt.subplots_adjust(top=0.9)
+ax.bar(x2, y2)
+plt.show()
